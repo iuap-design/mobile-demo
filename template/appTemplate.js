81a89511aa66d462611a30ab628ee6f8f3441639
liushaozhen <804199994@qq.com>
2019-12-10 20:09:37 +0800

    使用ucf

--- template/appTemplate.js
+++ template/appTemplate.js
@@ -1,15 +1,16 @@
+/**
+ * 整个应用的入口，包含路由，数据管理加载
+ */
+
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Link, Route } from 'react-router-dom';
 replaceImportant
-import 'antd-mobile/dist/antd-mobile.css'
-import './app.less'
-
+import "./app.less";
 
 class App extends Component{
     render(){
         return (<div className='link'>
-        <Link to="/">首页</Link>
         replaceLink
     </div>)
     }
@@ -26,8 +27,5 @@ class Routers extends Component{
         )
     }
 }
-
-
-
 
 replaceRender
